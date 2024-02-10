cdd9618f03c4e9135b99d937203e5f09d16ec94c
jonas089 <jonas089@gmail.com>
2024-02-11 00:30:40 +0100

    fix script

--- build-nctl.sh
+++ build-nctl.sh
@@ -1,2 +1,2 @@
-cd ./nctl-titano-env/custom/ && ./build.sh && ./init-container.sh && sleep 50 && ./cp-users.sh && cp ./nctl-titano-env/custom/users ./users
+cd ./nctl-titano-env/custom/ && ./build.sh && ./init-container.sh && sleep 50 && ./cp-users.sh && sleep 5 && cp ./nctl-titano-env/custom/users -r ./users
 
