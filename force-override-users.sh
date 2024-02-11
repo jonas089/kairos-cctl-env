1a218b9dda5b5ff307bb338b95f5401aa417809a
jonas089 <jonas089@gmail.com>
2024-02-11 01:41:33 +0100

    fix bugs

--- force-override-users.sh
+++ force-override-users.sh
@@ -1,5 +1,5 @@
 # Remove the directory
-docker exec casper-nctl-container /bin/bash -c 'sudo rm -rf /home/casper/casper-node/utils/nctl/assets/' && \
+docker exec casper-nctl-container /bin/bash -c 'sudo rm -rf /home/casper/casper-node/utils/nctl/assets/net-1/users' && \
 # Create the directory again (no need for `docker mkdir`, just use `mkdir` in a bash command)
 docker exec casper-nctl-container /bin/bash -c 'mkdir -p /home/casper/casper-node/utils/nctl/assets/net-1/users' && \
 # Copy the 'users' directory to the created directory in the container
