808864fa6a0eeaf2932e189d0ee70b6387e13fa2
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-02-17 19:21:52 +0100

    update setup deps

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-git clone -b v2.0 --single-branch https://github.com/jonas089/purse-contract-cli.git
+git clone -b last-stable-v2.0 --single-branch https://github.com/jonas089/purse-contract-cli.git
 git clone -b v2.0 --single-branch https://github.com/jonas089/purse-contract-1.x.git
-git clone -b feat-track-node-2.0 --single-branch https://github.com/jonas089/casper-client-rs.git
+git clone -b last-stable-feat-track-node-2.0 --single-branch https://github.com/jonas089/casper-client-rs.git
