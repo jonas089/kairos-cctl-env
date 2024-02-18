344b22814f79af628db0c12ac8adf02b533e0d9f
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-02-18 16:05:56 +0100

    cleanup

--- setup-cctl.sh
+++ setup-cctl.sh
@@ -2,7 +2,7 @@
 python3 -m pip install supervisor toml tomlkit
 
 # Clone repos.
-git clone -b feat-2.0 --single-branch https://github.com/jonas089/casper-node.git && cd casper-node && git checkout 0bcc25f4a203be4e78eed679b54127ab1ed848ab && git pull origin 0bcc25f4a203be4e78eed679b54127ab1ed848ab && cd ../
+git clone -b feat-2.0 --single-branch https://github.com/casper-network/casper-node.git
 git clone https://github.com/casper-network/casper-node-launcher.git
 git clone -b last-stable-feat-track-node-2.0 https://github.com/jonas089/casper-client-rs.git
 git clone https://github.com/casper-network/cctl.git
