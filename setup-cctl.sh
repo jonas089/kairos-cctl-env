25eac79ac0fc01891cc03b5b5173e079c023e641
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-02-29 17:45:51 +0100

    fetch from cspr-rad

--- setup-cctl.sh
+++ setup-cctl.sh
@@ -2,9 +2,9 @@
 python3 -m pip install supervisor toml tomlkit
 
 # Clone repos.
-git clone -b feat-2.0 --single-branch https://github.com/casper-network/casper-node.git
+git clone -b kairos-feat-2.0 --single-branch https://github.com/cspr-rad/casper-node
 git clone https://github.com/casper-network/casper-node-launcher.git
-git clone -b last-stable-feat-track-node-2.0 https://github.com/jonas089/casper-client-rs.git
+git clone -b kairos-feat-track-2.0 --single-branch https://github.com/cspr-rad/casper-client-rs
 git clone https://github.com/casper-network/cctl.git
 
 # Install rust toolchain.
--- setup.sh
+++ setup.sh
@@ -1,2 +1,1 @@
-git clone -b last-stable-v2.0 --single-branch https://github.com/jonas089/kairos-contract-cli.git
-git clone -b v2.0 --single-branch https://github.com/jonas089/kairos-deposit-contract.git
+git clone -b feature/deposit-contract --single-branch git@github.com:jonas089/kairos-deposit-contract
