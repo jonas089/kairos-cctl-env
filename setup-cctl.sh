e00a35bc082082d87515362200b73cac9f63cd40
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-03-12 16:53:40 +0100

    update for kairos 1.5.6

--- setup-cctl.sh
+++ setup-cctl.sh
@@ -2,9 +2,9 @@
 python3 -m pip install supervisor toml tomlkit
 
 # Clone repos.
-git clone -b kairos-2.0 --single-branch https://github.com/jonas089/casper-node
+git clone -b kairos-1.5.6-risc0 --single-branch https://github.com/jonas089/casper-node
 git clone https://github.com/casper-network/casper-node-launcher.git
-git clone -b kairos-2.0 --single-branch https://github.com/jonas089/casper-client-rs
+git clone -b kairos-1.5.6 --single-branch https://github.com/jonas089/casper-client-rs
 git clone https://github.com/casper-network/cctl.git
 
 # Install rust toolchain.
